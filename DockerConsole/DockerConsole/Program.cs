96f3e50c3bc144099fd50139544eaf00c5a5c5e8
Nizam-labs <nizam.connect@outlook.com>
2025-10-21 12:18:09 +0530

    uploaded the docker compose application

---
+++ DockerConsole/DockerConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DockerConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Docker World!");
+            Console.WriteLine("This is console");
+            Console.WriteLine("Press enter to exit");
+
+            Console.ReadLine();
+        }
+    }
+}
